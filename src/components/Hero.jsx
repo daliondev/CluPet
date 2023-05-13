4470c629bad208ed74c14327ec6d8ae11e0424b4
daliondev <d4lion.dev@gmail.com>
2023-05-13 12:25:10 -0500

    feat: Change resgistration form

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,13 +18,13 @@ const Hero = () => {
                   name="email"
                   id="email"
                   className="bg-[#be9d63]  text-white rounded-lg block w-full p-2.5 placeholder-white"
-                  placeholder="clupet@gmail.com"
+                  placeholder="ejemplo@gmail.com"
                   required=""
                 />
               </div>
               <div>
                 <label className="block mb-2 text-sm font-medium text-[#502d16ff]">
-                  Password
+                  Contraseña
                 </label>
                 <input
                   type="password"
@@ -34,7 +34,18 @@ const Hero = () => {
                   required=""
                 />
               </div>
-
+              <div>
+                <label className="block mb-2 text-sm font-medium text-[#502d16ff]">
+                  Confirmar contraseña
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  className="bg-[#be9d63] rounded-lg block w-full p-2.5 placeholder-white"
+                  placeholder="••••••••••"
+                  required=""
+                />
+              </div>
               <button
                 type="submit"
                 className="bg-[#605137] w-full text-white bg-primary-600 hover:bg-[#746243] font-medium rounded-lg text-sm px-5 py-2.5 text-center"
