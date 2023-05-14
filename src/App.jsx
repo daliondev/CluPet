c918ae40993cb24c5c1ae4a299c75fd14dd601ef
daliondev <d4lion.dev@gmail.com>
2023-05-13 21:51:12 -0500

    feat: Description section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import "./App.css"
 import Header from "./components/Header"
 import Hero from "./components/hero"
+import Descripcion from "./components/Descripcion"
 
 function App() {
   return (
     <>
-      <main className="">
-        <section className="hero">
+      <main>
+        <section id="top-section">
           <Header />
           <Hero />
+          <Descripcion id="#descripcion" />
         </section>
       </main>
     </>
