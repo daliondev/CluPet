7aef03f39df3d10883cdd199c1f2b02c1f807cb0
daliondev <d4lion.dev@gmail.com>
2023-05-15 19:17:23 -0500

    new!(routing): React router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import "./App.css"
-import Header from "./components/Header/Header"
-import Hero from "./components/Hero/Hero"
-import Descripcion from "./components/Description/Descripcion"
+import Inicio from "./routes/inicio"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 function App() {
   return (
-    <>
-      <main>
-        <section id="top-section">
-          <Header />
-          <Hero />
-          <Descripcion />
-        </section>
-      </main>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/routes/inicio.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header/Header"
+import Hero from "../components/Hero/Hero"
+import Descripcion from "../components/Description/Descripcion"
+
+const Inicio = () => {
+  return (
+    <section id="top-section">
+      <Header />
+      <Hero />
+      <Descripcion />
+    </section>
+  )
+}
+
+export default Inicio
