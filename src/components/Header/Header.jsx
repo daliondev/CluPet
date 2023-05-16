d31beb61c7980ad4ef3eba498dac89894b1522a4
daliondev <d4lion.dev@gmail.com>
2023-05-15 20:11:07 -0500

    feat(adaptation): To description route

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
         <ul className="mr-5 pb-1 font-medium font-lexend text-base  ">
           {navlinks.map((navlink, index) => (
             <a
-              href={"#" + navlink}
+              href={index != 1 ? "/" + navlink : "#" + navlink}
               className="mr-5 pb-1 hover:text-[#615e58] hover:border-b border-[#502d16ff]"
               key={index}
             >
