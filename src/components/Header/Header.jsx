8334aefab8d781584289d4c2a92de7a776cfe1ec
daliondev <d4lion.dev@gmail.com>
2023-05-17 21:30:05 -0500

    feat(refactor): Links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import logo from "../../assets/Logo.svg"
-import navlinks from "../../constants/navlinks.js"
-import { Link } from "react-router-dom"
+import Links from "./components/links"
 
 const Header = () => {
   return (
@@ -8,26 +7,17 @@ const Header = () => {
       <div className="flex items-center h-20 mt-2">
         <img src={logo} className="sm:w-16 sm:mr-2 sm:ml-9 mt-2 w-10 ml-5 " />
         <div>
-          <p
+          <a
             className="sm:self-start sm:font-lexend sm:text-5xl text-3xl font-thin "
             id="title-logo"
+            href="/"
           >
             CluPet
-          </p>
+          </a>
         </div>
       </div>
       <div className="sm:flex justify-between items-center hidden">
-        <ul className="mr-5 pb-1 font-medium font-lexend text-base  ">
-          {navlinks.map((navlink, index) => (
-            <Link
-              to={index != 1 ? "/" + navlink : "#" + navlink}
-              className="mr-5 pb-1 hover:text-[#615e58] hover:border-b border-[#502d16ff]"
-              key={index}
-            >
-              {navlink}
-            </Link>
-          ))}
-        </ul>
+        <Links />
       </div>
     </div>
   )
